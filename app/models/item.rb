f72205604d7b9b77e09d00778b818de798740a29
Janet Mendez <janetmendez.03@gmail.com>
2019-08-20 17:14:45 -0400

    fixed variable names

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  has_many :offers_made, foreign_key: :offered_item_id, class_name: 'Offer'
-  has_many :offered_to, through: :offers_made, :source => :wanted_item
+  has_many :created_offers, foreign_key: :offered_item_id, class_name: 'Offer'
+  has_many :created_offers_items, through: :created_offers, :source => :wanted_item
   
-  has_many :offers_recieved, foreign_key: :wanted_item_id, class_name: 'Offer'
-  has_many :offered_items, through: :offers_recieved, :source => :offered_item
+  has_many :recieved_offers, foreign_key: :wanted_item_id, class_name: 'Offer'
+  has_many :recieved_offers_items, through: :recieved_offers, :source => :offered_item
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     has_many :items
 
-    has_many :reviews_made, foreign_key: :reviewer_id, class_name: 'Review'
-    has_many :reviewed, through: :reviews_made, :source => :reviewee
+    has_many :created_reviews, foreign_key: :reviewer_id, class_name: 'Review'
+    has_many :reviewed, through: :created_reviews, :source => :reviewee
     
-    has_many :reviews_recieved, foreign_key: :reviewee_id, class_name: 'Review'
-    has_many :reviewers, through: :reviews_recieved, :source => :reviewer
+    has_many :recieved_reviews, foreign_key: :reviewee_id, class_name: 'Review'
+    has_many :reviewers, through: :recieved_reviews, :source => :reviewer
 
     has_many :question_answers, through: :items
 
