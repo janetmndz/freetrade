3bcf6a89718603c9e6813607009eb50ef617f528
Janet Mendez <janetmendez.03@gmail.com>
2019-08-20 14:56:11 -0400

    self-ref fixed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  # has_many :offered_items, foreign_key: :offered_item_id, class_name: 'Offer'
-  # has_many :offered_offers, through: :offered_items, :source => :offered_item
   has_many :offers_made, foreign_key: :offered_item_id, class_name: 'Offer'
   has_many :offered_to, through: :offers_made, :source => :wanted_item
   
-
-  # has_many :wanted_items, foreign_key: :wanted_item_id, class_name: 'Offer'
-  # has_many :wanted_offers, through: :wanted_items, :source => :wanted_item
   has_many :offers_recieved, foreign_key: :wanted_item_id, class_name: 'Offer'
   has_many :offered_items, through: :offers_recieved, :source => :offered_item
 
