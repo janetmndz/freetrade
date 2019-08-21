0cdf2684877f0e45245058c70c6a4611a78e0018
Gulnoza Muminova <gmcharmy@gmail.com>
2019-08-21 12:04:15 -0400

    Athorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+# before_action :set_up_auth_variables
+
+    def set_up_auth_variables
+        
+        @user_id = session["user_id"]
+        @loged_in = !!session["user_id"]
+        if @loged_in
+           @current_user=User.find(@user_id)
+        end
+    end
+
+
+    def autorized
+        
+        if !!@logged_in
+            return redirect_to welcome_path
+        end
+
+    end
+
+    def logout
+        session["user_id"]=nil
+    end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,12 +1,18 @@
 class LoginController < ApplicationController
+    
     def new
         
     end
     def create
         @user = User.find_by(email: params[:email])
         if @user && @user.authenticate(params[:password])
+            #set session["user_id"]
+            session["user_id"] = @user.id
+            
+            
             flash[:message] = "Logging in #{@user.name}"
             redirect_to items_path
+
         else
             flash[:message] = "Incorrect email or password"
             redirect_to welcome_path
